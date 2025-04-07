da896c3ba4cf655b97c5ef57483c5a681a198f78
flavius75 <bicaadr@yahoo.com>
2025-04-07 17:07:04 +0200

    (backend) Add first test

---
+++ backend/tests/test_main.py
@@ -0,0 +1,9 @@
+from fastapi.testclient import TestClient
+from main import app
+
+client = TestClient(app)
+
+def test_hello_endpoint():
+    response = client.get("/hello")
+    assert response.status_code == 200
+    assert response.json() == {"message": "Hello"}
